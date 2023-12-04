b46947f1e946a9891859cb87c1324b33f219840e
mcrachel7 <mcrachel79@unitec.edu>
2023-12-04 01:49:41 -0600

    last commit

---
+++ src/empleado.java
@@ -0,0 +1,25 @@
+
+/**
+ *
+ * @author Rachel
+ */
+public class empleado {
+    int id;
+    String nombre;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
